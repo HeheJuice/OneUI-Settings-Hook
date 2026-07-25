48a7819e63060337afdacff3393c808f34463ac2
𝐇𝐞𝐡𝐞𝐉𝐮𝐢𝐜𝐞 <minecraftplayer11964@gmail.com>
2026-07-25 20:21:42 +0800

    Update SettingsActivity.kt

--- app/src/main/java/com/HeheJuice/OneUISettingsHook/SettingsActivity.kt
+++ app/src/main/java/com/HeheJuice/OneUISettingsHook/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.HeheJuice.OneUISettingsHook
 
 import android.app.Activity
+import android.content.ComponentName
 import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Color
